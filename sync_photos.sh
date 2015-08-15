bd7df75d95d84f8f28c15a950a38ab11e4bdc611
Jason Mahony <jason.mahony@autodesk.com>
2015-08-14 17:29:31 -0700

    more rsync options

--- sync_photos.sh
+++ sync_photos.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rsync -ahvz --progress /Users/jason/Pictures/Photos\ Library.photoslibrary /Volumes/jason/Pictures/Photos\ Library\ Jason.photoslibrary
+rsync -av --progress /Users/jason/Pictures/Photos\ Library.photoslibrary /Volumes/jason/Pictures/Photos\ Library\ Jason.photoslibrary
