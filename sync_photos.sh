e31ecf36b8ccd764c4b17c4cef2477543dbfcea2
Jason Mahony <jason.mahony@autodesk.com>
2015-10-16 16:24:37 -0700

    improve photo backup script

--- sync_photos.sh
+++ sync_photos.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rsync -a --delete --progress /Users/jason/Pictures/Photos\ Library.photoslibrary /Volumes/jason/Pictures/Photos\ Library\ Jason.photoslibrary
+rsync -aP --delete /Users/jason/Pictures/Photos\ Library.photoslibrary /Volumes/jason/Pictures/jason_backup_photos/
