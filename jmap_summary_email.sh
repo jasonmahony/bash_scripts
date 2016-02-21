9b62a89fe779cc5ae950009d60565183501d3531
Jason Mahony <jason.mahony@gmail.com>
2016-02-21 06:54:59 -0800

    clean up jmap script

--- jmap_summary_email.sh
+++ jmap_summary_email.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Run jmap on elasticsearch host and email a summary
+
 NOW=`date +"%Y-%m%d_%m%S"`
 HOST=`host $1 | awk {'print$5'}`
 ssh opsadm@$1 "ps -ef | grep java | grep -v grep | awk {'print\$2'} | xargs -I {} sudo -u elasticsearch /usr/bin/jmap -heap {} | sudo -u elasticsearch tee /var/log/es/jmap/jmap.out.$NOW >/dev/null"
